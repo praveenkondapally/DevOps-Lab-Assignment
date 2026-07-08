120167fa86c6e4c709363cc5b7dc959ea6c3f123
praveenkondapally <43089438+praveenkondapally@users.noreply.github.com>
2026-07-08 11:26:33 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to DevOps Lab");
+        System.out.println("Application built using Maven");
+        System.out.println("Containerized using Docker");
+    }
+}
